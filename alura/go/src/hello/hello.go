d1e4d1379304933fb508110d82c311bc6ef76f18
prisciladuarte <duartecostap@gmail.com>
2022-02-17 16:56:59 -0300

    Variáveis

---
+++ alura/go/src/hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Douglas"
+	var idade int = 24
+	var versao float32 = 1.1
+
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão ", versao)
+	fmt.Println("Olá, sr.", nome, "Sua idade é", idade)
+}
